e0c6c7cfd158a5b9e1992f027e317e265e6dd5c5
skolotienko <sergey@kolotienko.ru>
2015-11-10 00:17:08 +0300

    Быстрый старт

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -10,13 +10,39 @@ using namespace model;
 using namespace std;
 
 void MyStrategy::move(const Car& self, const World& world, const Game& game, Move& move) {
-    move.setEnginePower(1.0);
-    move.setThrowProjectile(true);
-    move.setSpillOil(true);
+	double nextWaypointX = (self.getNextWaypointX() + 0.5) * game.getTrackTileSize();
+	double nextWaypointY = (self.getNextWaypointY() + 0.5) * game.getTrackTileSize();
 
-    if (world.getTick() > game.getInitialFreezeDurationTicks()) {
-        move.setUseNitro(true);
-    }
+	double cornerTileOffset = 0.25 * game.getTrackTileSize();
+
+	switch (world.getTilesXY()[self.getNextWaypointX()][self.getNextWaypointY()]) {
+		case LEFT_TOP_CORNER:
+			nextWaypointX += cornerTileOffset;
+			nextWaypointY += cornerTileOffset;
+			break;
+		case RIGHT_TOP_CORNER:
+			nextWaypointX -= cornerTileOffset;
+			nextWaypointY += cornerTileOffset;
+			break;
+		case LEFT_BOTTOM_CORNER:
+			nextWaypointX += cornerTileOffset;
+			nextWaypointY -= cornerTileOffset;
+			break;
+		case RIGHT_BOTTOM_CORNER:
+			nextWaypointX -= cornerTileOffset;
+			nextWaypointY -= cornerTileOffset;
+			break;
+	}
+
+	double angleToWaypoint = self.getAngleTo(nextWaypointX, nextWaypointY);
+	double speedModule = hypot(self.getSpeedX(), self.getSpeedY());
+
+	move.setWheelTurn(angleToWaypoint * 32.0 / PI);
+	move.setEnginePower(0.75);
+
+	if (speedModule * speedModule * abs(angleToWaypoint) > 2.5 * 2.5 * PI) {
+		move.setBrake(true);
+	}
 }
 
 MyStrategy::MyStrategy() { }
