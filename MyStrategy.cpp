a807eba15fd9b6c2e29d99f4a96c18da4026cade
skolotienko <sergey@kolotienko.ru>
2015-11-10 23:10:21 +0300

    Добавил CVector2D. Переделал быструю стратегию на его использование.

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -6,37 +6,49 @@
 #include <cmath>
 #include <cstdlib>
 
+#include "engine/Vector2D.h"
+
 using namespace model;
 using namespace std;
+using namespace Engine;
 
-void MyStrategy::move(const Car& self, const World& world, const Game& game, Move& move) {
+void MyStrategy::move(const Car& self, const World& world, const Game& game, Move& move)
+{
 	double nextWaypointX = (self.getNextWaypointX() + 0.5) * game.getTrackTileSize();
 	double nextWaypointY = (self.getNextWaypointY() + 0.5) * game.getTrackTileSize();
+	CVector2D nextWaypoint(nextWaypointX, nextWaypointY);
 
 	double cornerTileOffset = 0.25 * game.getTrackTileSize();
 
 	switch (world.getTilesXY()[self.getNextWaypointX()][self.getNextWaypointY()]) {
-		case LEFT_TOP_CORNER:
-			nextWaypointX += cornerTileOffset;
-			nextWaypointY += cornerTileOffset;
-			break;
-		case RIGHT_TOP_CORNER:
-			nextWaypointX -= cornerTileOffset;
-			nextWaypointY += cornerTileOffset;
-			break;
-		case LEFT_BOTTOM_CORNER:
-			nextWaypointX += cornerTileOffset;
-			nextWaypointY -= cornerTileOffset;
-			break;
-		case RIGHT_BOTTOM_CORNER:
-			nextWaypointX -= cornerTileOffset;
-			nextWaypointY -= cornerTileOffset;
-			break;
+	case LEFT_TOP_CORNER:
+		nextWaypoint.Add(cornerTileOffset, cornerTileOffset);
+		break;
+	case RIGHT_TOP_CORNER:
+		nextWaypoint.Add(-cornerTileOffset, cornerTileOffset);
+		break;
+	case LEFT_BOTTOM_CORNER:
+		nextWaypoint.Add(cornerTileOffset, -cornerTileOffset);
+		break;
+	case RIGHT_BOTTOM_CORNER:
+		nextWaypoint.Add(-cornerTileOffset, -cornerTileOffset);
+		break;
 	}
 
-	double angleToWaypoint = self.getAngleTo(nextWaypointX, nextWaypointY);
-	double speedModule = hypot(self.getSpeedX(), self.getSpeedY());
+	double debugAngleToWaypoint = self.getAngleTo(nextWaypointX, nextWaypointY);
+	double debugSpeedModule = hypot(self.getSpeedX(), self.getSpeedY());
+	debugAngleToWaypoint;
+	debugSpeedModule;
 
+	CVector2D selfVector(self.getX(), self.getY());
+	CVector2D selfToWaypointVector(nextWaypoint);
+	selfToWaypointVector.Subtract(selfVector);
+	CVector2D selfUnitVector(1, 0);
+	selfUnitVector.Rotate(self.getAngle());
+	double angleToWaypoint = selfUnitVector.GetAngle(selfToWaypointVector);
+	CVector2D selfSpeedVector(self.getSpeedX(), self.getSpeedY());
+	double speedModule = selfSpeedVector.GetLength();
+	
 	move.setWheelTurn(angleToWaypoint * 32.0 / PI);
 	move.setEnginePower(0.75);
 
