cb701aa1b753bf3c092feef688e168eabab2d42d
skolotienko <sergey@kolotienko.ru>
2015-11-25 02:37:23 +0300

    Уменьшил глубину перебора, увеличил ширину. Теперь более аккуратно проезжаем резкие повороты. Ранее с такими же параметрами не было хорошего результата из-за того, что не было сохранения предыдущей последовательности действий.

--- CodeRacing/BestMoveFinder.h
+++ CodeRacing/BestMoveFinder.h
@@ -69,7 +69,7 @@ private:
 	std::vector<CState> stateCache;
 
 	static const std::vector<std::pair<std::vector<CMyMove>, std::vector<int>>> allMovesWithLengths;
-	static const int maxTick = 175;
+	static const int maxTick = 125;
 
 	void processPreviousMoveList();
 	void processMoveIndex(size_t moveIndex, const std::vector<CMoveWithDuration>& prevMoveList);
